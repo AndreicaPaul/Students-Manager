2133b6c31a5fd1e34d47481965c01218ecaf9d3e
Andreica Paul <paulpaulandreica@gmail.com>
2016-12-14 15:58:52 +0200

    Added show menu, read input

--- StudentsManager/src/com/company/Main.java
+++ StudentsManager/src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.io.Console;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String userInput = "";
+        Menu menu = new Menu();
+        menu.showMenu();
+
+        System.out.println("Please input your choice: ");
+        Scanner scanner = new Scanner(System.in);
+        userInput = scanner.nextLine();
+
+        switch (userInput) {
+            case "1": {
+                System.out.println("Your choice is 1");
+                break;
+            }
+            case "2": {
+                break;
+            }
+            case "3": {
+                break;
+            }
+            default: {
+                //Ask for valid input from user
+                break;
+            }
+        }
     }
 }
---
+++ StudentsManager/src/com/company/Menu.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by itbrainiacs1 on 12/14/16.
+ */
+public class Menu {
+    public void showMenu() {
+        System.out.println("1. Add Student");
+        System.out.println("2. View all Students");
+        System.out.println("3. Remove Student");
+        System.out.println("4. Add Discipline");
+    }
+}
